3f16c1c6fd03345d71f27ab6d241b45d8026c436
octobrekids <chonrada.k@mail.kmutt.ac.th>
2019-11-16 20:55:38 +0700

    query/selector/getelementby

---
+++ Chapter_6/sandbox.js
@@ -0,0 +1,50 @@
+// Document Object Model
+// gets only the first one it found
+// const para = document.querySelector('p')
+
+// . (class)
+// const para = document.querySelector('.error')
+
+// const para = document.querySelector('div.error')
+
+// get unique selector from the element
+// const para = document.querySelector('body > h1')
+// console.log(para)
+
+// NODE LIST
+
+// const paras = document.querySelectorAll('p')
+// const error = document.querySelectorAll('.error')
+
+// console.log(paras) // node list not array but can use dot notation to access
+// console.log(error)
+
+// console.log(paras[2])
+
+// can use foreach too ! 
+// paras.forEach(para => {
+//     console.log(para)
+// })
+
+// ------------------------
+// get element by ID
+// const title = document.getElementById('page-title')
+// console.log(title)
+
+// get elements by their class name (cant use foreach method in HTML collection!)
+// const errors = document.getElementsByClassName('error')
+// console.log(errors)
+// console.log(errors[0])
+
+// get elements by their tag name
+// const paras = document.getElementsByTagName('p')
+// console.log(paras)
+// console.log(paras[1])
+
+
+
+
+
+
+
+
